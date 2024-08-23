ee70e590ff4d9b7dea403130e6d09b7ffcb4026c
Bolynskii Ewgenii <119881162+MrEwgenius@users.noreply.github.com>
2024-08-23 13:40:12 +0300

    add Suspense

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,36 +1,41 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import styles from "./Results.module.scss";
-
 import CountUp from "react-countup";
 import { useRouter, useSearchParams } from "next/navigation";
 
+function Search() {
+    const searchParams = useSearchParams();
+    const potatoSacks = searchParams?.get("potatoSacks");
+
+    return (
+        <span>
+            <CountUp start={0} end={potatoSacks ? +potatoSacks : 0} />
+        </span>
+    );
+}
+
 const Results = () => {
     const router = useRouter();
-    const searchParams = useSearchParams();
     const onTitleClick = () => {
         router.push("/");
     };
 
-    const potatoSacks = searchParams?.get("potatoSacks");
-
     return (
-        <div className={styles.containerResults}>
-            <div className={styles.textContent}>
-                <h2 className={styles.title}>
-                    Ты оцениваешься в
-                    {potatoSacks && (
-                        <span>
-                            <CountUp start={0} end={+potatoSacks} />
-                        </span>
-                    )}
-                </h2>
-                <p className={styles.subTitle}>Мешков(-а) картошки</p>
+        <Suspense fallback={<div>Загрузка...</div>}>
+            <div className={styles.containerResults}>
+                <div className={styles.textContent}>
+                    <h2 className={styles.title}>
+                        Ты оцениваешься в <Search />
+                    </h2>
+                    <p className={styles.subTitle}>Мешков(-а) картошки</p>
+                </div>
+                <button onClick={onTitleClick} className={styles.button}>
+                    РАССЧИТАТЬ ЕЩЁ РАЗ?
+                </button>
             </div>
-            <button onClick={onTitleClick} className={styles.button}>
-                РАССЧИТАТЬ ЕЩЁ РАЗ?
-            </button>
-        </div>
+        </Suspense>
     );
 };
+
 export default Results;
